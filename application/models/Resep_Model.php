207c4968ad99c601c947b01bb8c3d1b15f612e9c
aliwafa16 <aliwafamuhamad16@gmail.com>
2021-07-01 20:10:30 +0700

    details

--- application/models/Resep_Model.php
+++ application/models/Resep_Model.php
@@ -4,15 +4,41 @@ use GuzzleHttp\Client;
 
 class Resep_Model extends CI_Model
 {
+    private $_client;
+
+    public function __construct()
+    {
+        $this->_client = new Client([
+            'base_uri' => 'http://localhost:3000/'
+        ]);
+    }
 
     public function F_getAllResep()
     {
-        $client = new Client();
-        $response = $client->request('GET', 'localhost:3000/resep/');
-
-
+        $response = $this->_client->request('GET', 'resep/');
         $result = json_decode($response->getBody()->getContents(), TRUE);
 
+        return $result;
+    }
+
+    public function F_getResepByID($id_resep)
+    {
+        $response = $this->_client->request('GET', 'resep/' . $id_resep);
+        $result = json_decode($response->getBody()->getContents(), TRUE);
+        return $result;
+    }
+
+    public function F_getBahanByID($id_resep)
+    {
+        $response = $this->_client->request('GET', 'bahan/r/' . $id_resep);
+        $result = json_decode($response->getBody()->getContents(), TRUE);
+        return $result;
+    }
+
+    public function F_getCaraMasakByID($id_resep)
+    {
+        $response = $this->_client->request('GET', 'masak/r/' . $id_resep);
+        $result = json_decode($response->getBody()->getContents(), TRUE);
         return $result;
     }
 }
